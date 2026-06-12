4d22ba614272b5302fc0256b5323fe742555eb32
Judah_Matthew <judahmatt.15@gmail.com>
2026-06-12 20:21:09 +0530

    Add files via upload

---
+++ Module/looper.py
@@ -0,0 +1,3 @@
+def loop(a,b):
+    for i in range(a,b):
+        print(i)
---
+++ Module/main.py
@@ -0,0 +1,4 @@
+import looper
+a=int(input("Enter Value For Starting Number : "))
+b=int(input("Enter Value For Ending Number  : "))
+looper.loop(a,b)
