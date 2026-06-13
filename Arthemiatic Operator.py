b6164af1b4d2607926c907351bb3c850b8fcb6d9
Judah_Matthew <judahmatt.15@gmail.com>
2026-06-13 20:25:13 +0530

    Add files via upload

---
+++ Arthemiatic Operator.py
@@ -0,0 +1,11 @@
+#====================================================
+a=float(input("Enter a nambar :"))
+b=float(input("Enter a nambar :"))
+print("Addition : ",a+b)
+print("Subracttion : ",a-b)
+print("Multiplication : ",a*b)
+print("Division : ",a/b)
+print("Modulus : ",a%b)
+print("Floor Division : ",a//b)
+print("Power : ",a**b)
+#=====================================================
