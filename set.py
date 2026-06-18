84b75753aa5be41d52c6d697397ec538284daa8c
Judah_Matthew <judahmatt.15@gmail.com>
2026-06-18 20:28:45 +0530

    Refactor set operations and restore print statements

--- set.py
+++ set.py
@@ -23,29 +23,4 @@ print(type(s))
 a = {1,2,3,4}
 b = {2,3,5,6}
 
-print(a.symmetric_difference(b))# s = {1,2,3}
-# print(s)
-
-# #Adding Element
-# s.add(4)
-# print(s)
-
-# #Removing element
-# s.pop()
-# print(s)
-
-# s.remove(3) #Removes the element but throws an error if it doesn't find it
-# #s.remove(44)
-# s.discard(44) #Doesn't throw an error
-# print(s)
-
-# #Set Methods and Frozen Set
-# s = frozenset({1,2,3,4,5})
-# print(len(s))
-# print(type(s))
-
-#Methods
-a = {1,2,3,4}
-b = {2,3,5,6}
-
 print(a.symmetric_difference(b))
